4d860e2898df89612625a50ad102bb36f759b3ae
Monika Jaworowska <mon.jaworowska@gmail.com>
2022-02-19 22:10:56 +0100

    WeatherAppComponent template done

---
+++ src/components/WeatherApp.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import EnterLocationForm from "./EnterLocationForm";
+
+const WEATHER_API_URL = "http://api.openweathermap.org/data/2.5";
+const GEO_API_URL = "http://api.openweathermap.org/geo/1.0/direct?q=";
+const API_KEY = "6a175ae0557a7f1a5308f4eaf4f1063f";
+
+function WeatherApp() {
+  const [location, setLocation] = useState();
+  return (
+    <Container maxWidth="md">
+      {console.log(location)}
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <EnterLocationForm />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card variant="outlined">2</Card>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card variant="outlined">3</Card>
+        </Grid>
+        <Grid container item spacing={3}>
+          <Grid item xs={12} md={3}>
+            <Card variant="outlined">4</Card>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <Card variant="outlined">5</Card>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <Card variant="outlined">6</Card>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <Card variant="outlined">6</Card>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
+
+export default WeatherApp;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import WeatherApp from "./components/WeatherApp";
 
 function App() {
-  return <>Weather App</>;
+  return <WeatherApp />;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
